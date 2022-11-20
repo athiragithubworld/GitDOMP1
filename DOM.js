af9d1f7cd62eff37f9ace5001706fade3bd24ff0
athiragithubworld <athirahrms@gmail.com>
2022-11-20 20:27:35 +0530

    Task-7 (5.FirstElementChild)

--- DOM.js
+++ DOM.js
@@ -103,19 +103,19 @@ header.style.borderBottom='solid 3px #000';
 // itemList.parentElement.style.backgroundColor = '#f4f4f4';
 // console.log(itemList.parentElement.parentElement.parentElement);
 
-// childNodes
-console.log(itemList.childNodes);
+// // childNodes
+// console.log(itemList.childNodes);
 
-console.log(itemList.children);
-console.log(itemList.children[1]);
-itemList.children[1].style.backgroundColor = 'yellow';
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
 
 // // // FirstChild
 //  console.log(itemList.firstChild);
 
-// // // firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello 1';
+// // firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
 
 
 // // lastChild
