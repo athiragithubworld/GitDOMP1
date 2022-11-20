5324f480f9778ac952170672cc45035f3cb3d781
athiragithubworld <athirahrms@gmail.com>
2022-11-20 20:26:01 +0530

    Tas-7(4.CreateChild)

--- DOM.js
+++ DOM.js
@@ -104,22 +104,22 @@ header.style.borderBottom='solid 3px #000';
 // console.log(itemList.parentElement.parentElement.parentElement);
 
 // childNodes
-// console.log(itemList.childNodes);
+console.log(itemList.childNodes);
 
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
 
-// // FirstChild
-// console.log(itemList.firstChild);
+// // // FirstChild
+//  console.log(itemList.firstChild);
 
-// // firstElementChild
+// // // firstElementChild
 // console.log(itemList.firstElementChild);
 // itemList.firstElementChild.textContent = 'Hello 1';
 
 
-// lastChild
-console.log(itemList.lastChild);
+// // lastChild
+// console.log(itemList.lastChild);
 
 // // lastElementChild
 // console.log(itemList.lastElementChild);
