15f24a53d55f76992d9b1a040ed3dd5fe7375c23
athiragithubworld <athirahrms@gmail.com>
2022-11-20 23:50:29 +0530

    Task-7(createelement, setAttribute, createtesxtnode ,appendchild,  add HEllo world before Item Lister, HEllo word before Item 1)

--- DOM.js
+++ DOM.js
@@ -93,10 +93,11 @@ header.style.borderBottom='solid 3px #000';
 // ------------------------------------------------------
 // // TRAVERSING THE DOM //
  var itemList = document.querySelector('#items');
-// parentNode
+// // parentNode
 // console.log(itemList.parentNode);
 // itemList.parentNode.style.backgroundColor = '#f4f4f4';
 // console.log(itemList.parentNode.parentNode.parentNode);
+// parentNode.innerhtml=parentNode.innerhtml
 
 // // parentElement
 // console.log(itemList.parentElement);
@@ -125,15 +126,50 @@ header.style.borderBottom='solid 3px #000';
 // console.log(itemList.lastElementChild);
 // itemList.lastElementChild.textContent = 'Hello 4';
 
-// nextSibling
- console.log(itemList.nextSibling);
+// // nextSibling
+//  console.log(itemList.nextSibling);
 
-// nextElementSibling
-console.log(itemList.nextElementSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
 
- // previousSibling
- console.log(itemList.previousSibling);
+//  // previousSibling
+//  console.log(itemList.previousSibling);
 
-// // previousElementSibling
- console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+// // // previousElementSibling
+//  console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
 
+// createElement
+
+// Create a div
+var newDiv =  document.createElement('div');
+
+// Add class
+newDiv.className= 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+
+// Query Selector
+
+// add HEllo word before Item 1
+var item= document.querySelector('.list-group-item');
+item.textContent='Hello world';
+// item[1].previousElementSibling.textContent='Hello';
