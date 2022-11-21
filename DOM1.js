9e6c503ea95f08a73aa295d3e04133019ef14470
athiragithubworld <athirahrms@gmail.com>
2022-11-21 08:22:52 +0530

    Task-8(Add Edit button)

--- DOM1.js
+++ DOM1.js
@@ -23,11 +23,28 @@ function addItem(e){
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
 
-  // Create del button element
-  var deleteBtn = document.createElement('button');
+  
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  //   create edit button element
+var editBtn = document.createElement('button');
+
+// Add classes to edit button
+editBtn.className = 'btn btn-dark btn-sm float-right edit';
+
+// Append text node
+editBtn.appendChild(document.createTextNode('Edit'));
+
+// Append button to li
+li.appendChild(editBtn);
+
+// Append li to list
+itemList.appendChild(li);
+// -----------------------------------------------------
+// Create del button element
+var deleteBtn = document.createElement('button');
+
+// Add classes to del button
+  deleteBtn.className = 'btn btn-danger mr-2 btn-sm float-right delete';
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
@@ -37,6 +54,7 @@ function addItem(e){
 
   // Append li to list
   itemList.appendChild(li);
+
 }
 
 // remove item
