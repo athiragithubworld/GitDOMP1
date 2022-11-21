14dd3adbaabdc7c04d0d3a6997feeeaaaf618a5c
athiragithubworld <athirahrms@gmail.com>
2022-11-21 19:08:04 +0530

    task-9(Use Filter , add description textbox,,add the description to the list,filter using list and description)

--- DOM1.js
+++ DOM1.js
@@ -6,15 +6,16 @@ var filter = document.getElementById('filter');
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-// // Filter event
-// filter.addEventListener('keyup', filterItems);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-// Add item
+// ----------------------------Add item------------------------------------
 function addItem(e){
   e.preventDefault();
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newItem1 = document.getElementById('itemid').value;
 
   // Create new li element
   var li = document.createElement('li');
@@ -22,6 +23,7 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem1));
 
   
 
@@ -57,7 +59,7 @@ var deleteBtn = document.createElement('button');
 
 }
 
-// remove item
+// -----------remove item---------------------------
 function removeItem(e){
 
     if (e.target.classList.contains('delete')){
@@ -66,6 +68,37 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// Fliter Items
+function filterItems(e) {
+    //  convert text to lowercase
+    var text=e.target.value.toLowerCase();
+    // console.log(text);
+    // get li
+    var items = itemList.getElementsByTagName('li');
+//   convert to an array
+Array.from(items).forEach(function(item){
+ var itemName = item.firstChild.textContent;
+ var itemName1 = item.childNodes[1].textContent;
+// console.log(itemName);
+if(itemName.toLowerCase().indexOf(text)!=-1  || itemName1.toLowerCase().indexOf(text)!=-1 ){
+    item.style.display='block';
+}
+else{
+    item.style.display='none';
+}
+});
+// Array.from(items).forEach(function(itemid){
+//     var itemName = itemid.firstChild.textContent;
+//    // console.log(itemName);
+//    if(itemName.toLowerCase().indexOf(text)!=-1){
+//     itemid.style.display='block';
+//    }
+//    else{
+//     itemid.style.display='none';
+//    }
+//    });
 }
 
 
